756b7bea52cb8ef511ac795e73524106daf9949d
Lauren <hello@lauren.nz>
2017-04-25 23:44:32 +1200

    Exercise 2.2
    Write a program that uses input to prompt a user for their name and
    then welcomes them.

---
+++ 2.2.py
@@ -0,0 +1,7 @@
+# Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
+
+name = input('What is your name?\n')
+print('Hello, ' + name)
+
+# Why does this throw an error?
+# NameError: name 'whateveryourinputwas' is not defined
